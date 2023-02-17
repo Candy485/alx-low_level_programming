94610f771045d2b5dec37239949cd66acd7722a7
Candy485 <candyprince485@gmail.com>
2023-02-17 05:13:59 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always when successful
+ */
+
+int main(void)
+{
+char abc;
+
+for (abc = 'a'; abc <= 'z'; abc++)
+{
+putchar(abc);
+}
+
+putchar('\n');
+return (0);
+
+}
