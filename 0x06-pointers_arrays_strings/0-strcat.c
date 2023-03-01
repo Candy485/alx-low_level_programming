b5d637479c64f173293aba92636f16e4fc1b5e86
Candy485 <candyprince485@gmail.com>
2023-03-01 09:17:08 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,23 @@
-lines(12 sloc)  284 Bytes
-
 #include "main.h"
+
 /**
- *_strcat - This is the main function for concatenating text
- *@dest: Thsi is whre to copy the text to.
- *@src: This is the source of the string
- *Return: pointer to the destination.
+ * _strcat - concatenates two strings
+ * @dest: first string to be added to
+ * @src: second string to be added
+ * Return: concatenated string
  */
+
 char *_strcat(char *dest, char *src)
 {
-strcat(dest, src);
-return (dest);
+	char *temp = dest;
+
+	for (; *temp != '\0'; temp++)
+		;
+	for (; *src != '\0'; src++)
+	{
+		*temp = *src;
+		temp++;
+	}
+	*temp = *src;
+	return (dest);
 }
