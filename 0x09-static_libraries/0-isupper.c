4d800a2ca43cde07cdd74d6158188b684994720c
Candy485 <candyprince485@gmail.com>
2023-03-10 10:48:56 -0800

    task 0 static library

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,23 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - checks for an uppercase character
- *
- * @c: letter to be checked
- *
- * Return: 1 (if uppercase) 0 (otherwise)
+ * _isupper - checks for uppercase character
+ * @c: character that is checked
+ * Return: if uppercase returns 0 otherwise
  */
 
 int _isupper(int c)
 {
-	char i;
-	int upper = 0;
-
-	for (i = 'A'; i <= 'Z'; i++)
-	{
-		if (i == c)
-			upper = 1;
-	}
-
-	return (upper);
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
 }
