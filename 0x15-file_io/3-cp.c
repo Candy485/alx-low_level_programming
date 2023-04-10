418052fa981eca323917d2faa3f1ac58f0430876
Candy485 <candyprince485@gmail.com>
2023-04-10 05:49:42 -0700

    task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,29 +25,11 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
-	{
-		if (fd_w < 0 || write(fd_w, buf, x) != x)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_r);
-			exit(99);
-		}
-	}
-	if (x < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	m = close(fd_r);
-	n = close(fd_w);
-	if (m < 0 || n < 0)
-	{
-		if (m < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+	while ((x = read(fd_r, bose fd %d\n", fd_r);
 		if (n < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
 	return (0);
+}
 }
